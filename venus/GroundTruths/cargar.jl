5c3e0738d0dde3e8460ca1e779b42f36517e5be8
Javi <10784040+Javi520@users.noreply.github.com>
2023-03-26 22:32:05 +0200

    Arreglao aparentemente
    la manera de acceder a la matriz, en funcion de las coordenadas de los patrones, era incorrecta. Las coordenadas en el eje X se corresponden con la j y las del eje y con la i en la siguiente representación:
    pixel1 = imagen[i, j]
    Las x's son las columnas y las y's, las filas

--- venus/GroundTruths/cargar.jl
+++ venus/GroundTruths/cargar.jl
@@ -7,8 +7,26 @@ using FileIO
 using Images
 using NaturalSort
 
+#David, comentalo sino
+using ImageView
+using Images
+
 include("funciones.jl")
 
+# Args
+
+# Número de imágenes a procesar
+numberOfImages = 134
+
+for i=1:(size(ARGS,1))
+    if ARGS[i] == "-numImages"
+        try
+            numberOfImages = ARGS[i+1]
+        catch
+            error("No argument left but expected the number of images to process")
+        end
+    end
+end 
 # Debe ser común a todos los PC's
 ccd("Venus_AA/venus/GroundTruths")
 
@@ -27,7 +45,7 @@ filter!(endswith(".lxyr"), files_and_dirs)
 global dataset = []
 
 #134 = Numero maximo que queremos leer
-for count in 1:134
+for count in 1:numberOfImages
     name = "img"*string(count)*".lxyr"
     if isfile(name) #Checkeamos si existe
         matrix = readdlm(name)
@@ -40,13 +58,13 @@ for count in 1:134
     count = count + 1
 end
 #Printeamos el dataset original
-#println(dataset)
+println(dataset[1])
 
 
 size_data = size(dataset, 1)
 
-dataset_4 = []
-#Ahora creamos una matriz que guarde lo que fijo son volcanes (4) y los que no (1)
+negative_dataset = []
+#Ahora creamos una matriz que guarde lo que fijo son volcanes (1) y los que no (4)
 for i=1:size_data
     aux = []
     for j=1:(size(dataset[i], 1))
@@ -54,11 +72,11 @@ for i=1:size_data
             push!(aux, dataset[i][j,:])
         end
     end
-    push!(dataset_4, aux)
+    push!(negative_dataset, aux)
 end
 
-dataset_1 = []
-#Ahora creamos una matriz que guarde lo que fijo son volcanes (4) y los que no (1)
+positive_dataset = []
+#Ahora creamos una matriz que guarde lo que fijo son volcanes (1) y los que no (4)
 for i=1:size_data
     aux = []
     for j=1:(size(dataset[i], 1))
@@ -66,14 +84,14 @@ for i=1:size_data
             push!(aux, dataset[i][j,:])
         end
     end
-    push!(dataset_1, aux)
+    push!(positive_dataset, aux)
 end
-#dataset_4[i] for i=1:size_data  [dataset[i,:] for i=1:size_data if dataset[i,1]==4.0]
-#dataset_1= [dataset[i,:] for i=1:size_data if dataset[i,1]==1.0]
+#negative_dataset[i] for i=1:size_data  [dataset[i,:] for i=1:size_data if dataset[i,1]==4.0]
+#positive_dataset= [dataset[i,:] for i=1:size_data if dataset[i,1]==1.0]
 #=
-println(dataset_4) #Se maneja con [fila][columna].
+println(negative_dataset) #Se maneja con [fila][columna].
 println()
-println(dataset_1) #Se maneja con [fila][columna].
+println(positive_dataset) #Se maneja con [fila][columna].
 println()
 #Ahora mismo el dataset es una matriz con todos los patrones
 =#
@@ -106,12 +124,13 @@ function loadFolderImages(folderName::String)
     filter!(endswith(".png"), files)
     println(files)
     for fileName in files
+        println("Loading filename: "*fileName)
         image = load(img_path*"/"*fileName);
             
         # Check that they are color images
         #@assert(isa(image, Array{RGBA{Normed{UInt8,8}},2}) || isa(image, Array{RGB{Normed{UInt8,8}},2}))
         # Add the image to the vector of images
-        push!(images, convert(Array{Float64}, gray.(Gray.(image))));
+        push!(images, (fileName, convert(Array{Float64}, gray.(Gray.(image)))));
     end;
 
     # Convert the images to arrays by broadcasting the conversion functions, and return the resulting vectors ||Solo nos interesa lo gris(imageToColorArray.(images), 
@@ -121,7 +140,7 @@ end;
 # Tiene todas las imagenes, 1 por fila 
 matrix = loadFolderImages(img_path)
 
-save("gray.png", colorview(Gray,matrix[1]))
+save("gray.png", colorview(Gray,matrix[1][2]))
 
 #display(plot(matrix))
 
@@ -129,29 +148,30 @@ positive_images = []
 negative_images = []
 
 for i=1:size_data
-    for j=1:(size(dataset_4[i], 1))
-        println(dataset_4[i][j])
-        recorte = dataset_4[i][j][2:4]
-        push!(negative_images, recortar(recorte, matrix[i]))
+    for j=1:(size(negative_dataset[i], 1))
+        println(negative_dataset[i][j])
+        println("on "*(matrix[i][1]))
+        recorte = negative_dataset[i][j][2:4]
+        push!(negative_images, (matrix[i][1], recortar(recorte, matrix[i][2])))
     end
 end
 
 for i=1:size_data
-    for j=1:(size(dataset_1[i], 1))
-        println(dataset_1[i][j])
-        recorte = dataset_1[i][j][2:4]
-        push!(positive_images, recortar(recorte, matrix[i]))
+    for j=1:(size(positive_dataset[i], 1))
+        println(positive_dataset[i][j])
+        recorte = positive_dataset[i][j][2:4]
+        push!(positive_images, (matrix[i][1], recortar(recorte, matrix[i][2])))
     end
 end
 
-display(positive_images[1])
+#display(positive_images[1])
 
-# Recorrer dataset_4
-    # Para cada dataset_4[i], aplicarle a images[i] los recortes y apilarlos en positive_images[] de la siguiente manera push!(positive_images, recorte)
+# Recorrer negative_dataset
+    # Para cada negative_dataset[i], aplicarle a images[i] los recortes y apilarlos en positive_images[] de la siguiente manera push!(positive_images, recorte)
 
 # matrix_c = matrix[1]
 
-# coord = [convert(UInt16,dataset_4[1][2]), convert(UInt16,dataset_4[1][3]), convert(UInt16,round(dataset_4[1][4])) ] #x, y, radius
+# coord = [convert(UInt16,negative_dataset[1][2]), convert(UInt16,negative_dataset[1][3]), convert(UInt16,round(negative_dataset[1][4])) ] #x, y, radius
 
 # matrix_cut = matrix_c[coord[1],coord[2]]
 
@@ -162,11 +182,12 @@ if !isdir("imagenes_positivas")
     mkdir("imagenes_positivas")
 end
 global cont = 1
-for image in positive_images
-# Guardar la imagen en el directorio
-name = "recorte"*string(cont)*".png"
-save(joinpath("imagenes_positivas", name), image)
-global cont = cont+1
+for (name2, image) in positive_images
+    # Guardar la imagen en el directorio
+    name = "recorte"*string(cont)*".png"
+    println("saving \""*name*"\" of image named :"*name2)
+    save(joinpath("imagenes_positivas", name), image)
+    global cont = cont+1
 end
 
 # Crear el directorio si no existe
@@ -174,10 +195,11 @@ if !isdir("imagenes_negativas")
     mkdir("imagenes_negativas")
 end
 global cont = 1
-for image in negative_images
-# Guardar la imagen en el directorio
-name = "recorte"*string(cont)*".png"
-save(joinpath("imagenes_negativas", name), image)
-global cont = cont+1
+for (name2, image) in negative_images
+    # Guardar la imagen en el directorio
+    name = "recorte"*string(cont)*".png"
+    println("saving \""*name*"\" of image named :"*name2)
+    save(joinpath("imagenes_negativas", name), image)
+    global cont = cont+1
 end
 
--- venus/GroundTruths/funciones.jl
+++ venus/GroundTruths/funciones.jl
@@ -5,18 +5,29 @@ function ccd(relativeLocation::String)
     cd(mycd*relativeLocation)
 end
 
-function recortar(area, imagen)
+function recortar(coords, imagen)
 
-    imshow(imagen)
+    #imshow(imagen)
     
     # El radio se redondea hacia arriba para abarcar más area de la que abarcariamos de poder considerar los flotantes
     image_size = size(imagen)
-    rounded_radius = Int(ceil(area[3])) * K_MULTIPLIER
+    rounded_radius = Int(ceil(coords[3])) * K_MULTIPLIER
     
-    start_x = Int(max(1, area[1] - rounded_radius))
-    end_x = Int(min(area[1]+rounded_radius, image_size[1]))
-    start_y = Int(max(1, area[2] - rounded_radius))
-    end_y = Int(min(area[2]+rounded_radius, image_size[2]))
+    start_x = Int(max(1, coords[1] - rounded_radius))
+    end_x = Int(min(coords[1]+rounded_radius, image_size[1]))
+    start_y = Int(max(1, coords[2] - rounded_radius))
+    end_y = Int(min(coords[2]+rounded_radius, image_size[2]))
     
-    return imagen[start_x:end_x, start_y:end_y]
+    println("recortar-> Recortando:\t"*string(start_x)*","*string(start_y)*")-----"*"("*string(end_x)*","*string(end_y)*")")
+
+    # Las imágenes están traspuestas, habrá que recortar acorde a ello y trasponer
+    # el resultado
+    #= #! transpose warning
+        This operation is intended for linear algebra usage
+        - for general data manipulation see [permutedims](@ref Base.permutedims),
+        which is non-recursive
+    =#
+    imagen2 = imagen[start_y:end_y, start_x:end_x]
+    #imagen2 = transpose(imagen2)
+    return imagen2
 end
