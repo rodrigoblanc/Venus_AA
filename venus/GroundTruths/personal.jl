25037e4c8347a874268abf746d3b6a6fa1f7ab09
david-canedo <davidcanedofer@gmail.com>
2023-04-07 10:47:39 +0200

    prueba

--- venus/GroundTruths/personal.jl
+++ venus/GroundTruths/personal.jl
@@ -1,5 +1,3 @@
 # Comment this error when done
 #error("There is a need to have this in \"personal.jl\" file, mycd=\"PATH_WITH_\\WHERE_IS_Venus_AA_BUT_WITHOUT_Venus_AA")
 mycd = "/mnt/d/UNIVERSIDAD/TERCERO/Segundo Cuatri/AA/Practica/"
-
-#Hola
